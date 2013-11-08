57f36b76cffe386457bf5b901fde2a6ed2bafea2
Uli Fuchs <android.kennenlernen@gmx.de>
2013-11-08 20:37:05 +0100

    comments

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
-3.Steuerelemente-benutzen
-=========================
+# 3.Steuerelemente-benutzen
 
-Auslesen von Eingaben und behandeln von Schaltern
+[Entwuf]
+
+Einleitung hier...
+
+----
+
+Klassische Variante in Methode 'onCreate':
+
+**MainActivity.java**
+
+		Button b = (Button) findViewById(R.id.btnRaeumeAuf);
+		b.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+
+				// Referenz auf Eingabezeile ermitteln  
+				EditText et = (EditText) findViewById(R.id.editText1);
+				// EingabeZeile leeren
+				et.setText("");
+
+				// Referenz auf TextView ermitteln
+				TextView tv = (TextView) findViewById(R.id.textView1);
+				// 
+				tv.setText("Nun bitte selbst texten.");
+				
+			}
+		});
+
+----
+
+In einem deklaritiven Kontext:
+
+**MainActivity.java**
+
+	public void onClick_DrueckDochMal(View v) {
+		
+		// Referenz auf Eingabezeile ermitteln  
+		EditText et = (EditText) findViewById(R.id.editText1);
+		// Eingabezeile auslesen
+		String text = et.getText().toString();
+
+		// Referenz auf TextView ermitteln
+		TextView tv = (TextView) findViewById(R.id.textView1);
+		// Text der EingabeZeile in die TextView schreiben
+		tv.setText(text);
+		
+	}
+
+**activity_main.xml**
+
+
+    <Button
+        android:id="@+id/button1"
+		.
+		.
+        android:onClick="btnOnClick"
+        android:text="Drück doch mal" />
+
--- src/com/example/steuerelemente/MainActivity.java
+++ src/com/example/steuerelemente/MainActivity.java
@@ -1,9 +1,16 @@
+/*
+ * Copyright (c) 2013 Uli Fuchs
+ * MIT licensed 
+ */
+
 package com.example.steuerelemente;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,8 +18,44 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		/*
+		 * Dies ist die klassische Variante, einen onClick-Handler zu 
+		 * installieren.
+		 * Hierbei wird über 'new OnClickListener()' eine anonyme Klasse
+		 * instanziert.
+		 * PRO:
+		 * Der 'OnClickListener' ließe sich als Klasse auslagern.
+		 * CON:
+		 * 1.Innerhalb der anonymen Klasse kann nicht auf den Kontext der 
+		 *   umgebenen Activity zugegriffen werden, heißt Klassen-Variablen.
+		 * 2.Beim Lesen, Verstehen eines Quelltextes ist jede zusätzliche
+		 *   Zeile eine zuviel. 
+		 *   Jede Zeile zuviel birgt zusätzliches Fehler-Risiko.
+		 *   Dies ist sollte aber jeder mit sich selbst ausmachen.  
+		 */
+		Button b = (Button) findViewById(R.id.btnRaeumeAuf);
+		b.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+
+				// Referenz auf Eingabezeile ermitteln  
+				EditText et = (EditText) findViewById(R.id.editText1);
+				// EingabeZeile leeren
+				et.setText("");
+
+				// Referenz auf TextView ermitteln
+				TextView tv = (TextView) findViewById(R.id.textView1);
+				// 
+				tv.setText("Nun bitte selbst texten.");
+				
+			}
+		});
+		
 	}
 
 	@Override
@@ -28,7 +71,7 @@ public class MainActivity extends Activity {
 	 * Diese sogenannten Handler sind stets manuell zu erstellen!
 	 * </br>
 	 * Die Verbindung zum Steuerelement wird in der jeweiligen 'activity_xxx.xml'
-	 * hergestellt per 'android:onClick="onClick_DrueckDochMal"'
+	 * per 'android:onClick="onClick_DrueckDochMal"' hergestellt. 
 	 *   
 	 * @param v - Referenz auf das Steuerelement, hier 'btnDrueckDochMal'
 	 */
@@ -39,7 +82,9 @@ public class MainActivity extends Activity {
 		// Eingabezeile auslesen
 		String text = et.getText().toString();
 
+		// Referenz auf TextView ermitteln
 		TextView tv = (TextView) findViewById(R.id.textView1);
+		// Text der EingabeZeile in die TextView schreiben
 		tv.setText(text);
 		
 	}
